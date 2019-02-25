9bc1da261f65f56ace01223628f6e0de95d0ebee
azaa1 <azaa.chi11@gmail.com>
2019-02-25 02:35:31 +0000

    files modified

--- null-resource.tf
+++ null-resource.tf
@@ -3,14 +3,13 @@ resource "null_resource" "userdata" {
     host = "${element(aws_instance.example.*.public_ip, 0)}"
     type = "ssh"
     user = "ec2-user"
-    private_key = "${file("/root/.ssh/id_rsa")}"
+    private_key = "${file("/home/ec2-user/.ssh/id_rsa")}"
   }
   provisioner "remote-exec" {
     inline = [
       "sudo yum install httpd -y",
       "sudo yum install telnet -y",
       "sudo systemctl start httpd",
-      "sudo echo hello-word >> /var/www/html/index.html",
     ]
   }
 }
